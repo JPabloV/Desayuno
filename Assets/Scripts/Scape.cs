777a3dca2d1290b8e6055d7da4b747a877f6f7ad
JPabloV <70948164+JPabloV@users.noreply.github.com>
2021-03-12 12:43:19 +0100

    Escape con el boton y cambio imagen inicial.
    Escape con el boton y cambio imagen inicial.

---
+++ Assets/Scripts/Scape.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scape : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Debug.Log("Salimos de la aplicación.");
+            Application.Quit();
+        }
+    }
+}
